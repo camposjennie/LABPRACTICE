9a9e5f4db9837be7d25d45288425d86a45240c62
camposjennie <campos.jennie@csmstudents.org>
2019-05-17 01:48:20 +0000

    Update Labpractice.py

--- Labpractice.py
+++ Labpractice.py
@@ -1,8 +1,6 @@
 # LABPRACTICE
 print("the sky is blue")
-
 color=input("What is your favorite color?, Yellow, Blue, Lavender")
-
 if(color=="yellow"):
   print("My favorite cgt5f4racolor is yellow.")
 if(color=="blue"):
@@ -10,3 +8,4 @@ if(color=="blue"):
 if(color=="lavender"):
   print("My favorite color is lavender.")
 print("Hi Sister!!")
+print("Yellow is my favorite color and i like to write poetry and sing.")
