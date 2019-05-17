436c8e7dec1b9fc30032230581511e4a8d6b1fde
camposjennie <campos.jennie@csmstudents.org>
2019-05-17 15:16:49 +0000

    change

--- Labpractice.py
+++ Labpractice.py
@@ -1,27 +1,11 @@
 # LABPRACTICE
 print("the sky is blue")
-color=input("What is your favorite color?, Yellow, Blue, Lavender")
-if(color=="yellow"):
-  print("My favorite color is yellow.")
-if(color=="blue"):
-  print("My favorite color is blue.")
-if(color=="lavender"):
-  print("My favorite color is lavender.")
-
-print("Yes the grass is green and the sky is blue and the sun is yellow.") print("What is your favorite season?")
-
-season=input("What is your favorite season?")
-
-if(season=="SUMMER"):
-  print("I love the summer time.")
-if(seasons=="SPRING"):
-  print("I love the spring time.")
-if(seasons=="FALL"):
-  print("I love the fall.")
-if(seasons== "WINTER"):
-  print("I love the winter time.")
+print("My favoriet color is baby blue, lavender, and yellow")
+print("Yes the grass is green and the sky is blue and the sun is yellow.") 
+print("My favorite season is fall")
 
 print("I am 16 and  I like  Coffee and strawberry milkshakes")
 
 print("I like to write posm and doodle.")
 print("I love to hangout wiht my friends")
+
